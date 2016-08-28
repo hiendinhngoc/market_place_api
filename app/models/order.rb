56b3eb6bf6818d9562c9f6c88e07d4e00e4ce46e
dinhngochien <hiendinhngoc@yahoo.com.vn>
2016-08-28 11:39:10 +0700

    Adds quantity to placements

--- app/models/order.rb
+++ app/models/order.rb
@@ -16,10 +16,10 @@ class Order < ActiveRecord::Base
   end
 
   def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
-    product_ids_and_quantities.each do |product_id_and_quantities|
-      id, quantity = product_id_and_quantities
+    product_ids_and_quantities.each do |product_id_and_quantity|
+      id, quantity = product_id_and_quantity
 
-      self.placements.build(product_id: id)
+      self.placements.build(product_id: id, quantity: quantity)
     end
   end
 end
